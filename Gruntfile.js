d7c6f7c07e32ef5771b93c9491257c4ad2a7ac34
icit <icit@floater.local>
2014-06-08 22:11:04 +0100

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,21 @@
+module.exports = function (grunt) {
+
+    grunt.loadNpmTasks('grunt-contrib-connect');
+
+    grunt.initConfig({
+
+        connect: {
+            root: {
+                options: {
+                    keepalive: true,
+                    hostname: '*',
+                    port: 8005
+                }
+            }
+        }
+
+    });
+
+    grunt.registerTask('default', ['examples']);
+
+};
